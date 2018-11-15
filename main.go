c99599847a465f2f872951d3b89f56283a5b9822
cmnkumar <nkumar.cm@gmail.com>
2018-11-15 14:03:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import(
 func mainHandler() http.HandlerFunc{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello World!")
-		AWS_ACCESS_KEY_ID="AKIAIZTRXAHEBLAHKLAH"
+		AWS_ACCESS_KEY_ID="AKIAIZNXX3HERTTTVKKK"
 		AWS_ACCOUNT_ID="123456789012"
 	})
 }
