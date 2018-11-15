46b7d1309d44cda7bea6634aac8a34573dfcd975
cmnkumar <nkumar.cm@gmail.com>
2018-11-15 13:57:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func mainHandler() http.HandlerFunc{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello World!")
 		
-		AWS_ACCESS_KEY_ID="AKIAIZTRX3HEBLAHBLAH"
+		AWS_ACCESS_KEY_ID="AKIAIZTRX3HEBLAHB123"
 	})
 }
 
