da159881e5e829a7dbc2d764352493754561cc1c
cmnkumar <nkumar.cm@gmail.com>
2018-11-19 21:38:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import(
 
 func mainHandler() http.HandlerFunc{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello Worldc!")	
+		fmt.Fprintf(w, "Hello Worldck!")	
 	})
 }
 
