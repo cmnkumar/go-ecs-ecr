13e5400faf6095e9880fa52aab804f16273fab0a
cmnkumar <nkumar.cm@gmail.com>
2018-11-19 14:02:23 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import(
 func mainHandler() http.HandlerFunc{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello Worldcf!")
-	
-		
-		
+	AWS_ACCOUNT_ID=811558471755
+			
 		
 	})
 }
